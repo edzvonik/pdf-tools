191b845d53113f73b223d4bd0b5b8d2fe5fe9130
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2023-05-03 21:29:10 +0300

    Init project

---
+++ src/main/java/com/dzvonik/pdftools/PdfToolsApplication.java
@@ -0,0 +1,13 @@
+package com.dzvonik.pdftools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfToolsApplication.class, args);
+	}
+
+}
